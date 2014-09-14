0048575d6e9da7bb7282bc18f35c786dd40cf156
cwerner13 <bianalyst@operamail.com>
2014-09-14 20:18:56 +0200

    hyperlinks added

--- ui.R
+++ ui.R
@@ -8,14 +8,16 @@ shinyUI(fluidPage(
         sidebarPanel(
                 
                 
-                ('The IMDb Top 250 list celebrates outstanding films, cinema 
+                ('The'),  
+                tags$a(href="http://www.imdb.com/chart/top?ref_=nv_ch_250_4", "IMDb Top 250 list"),
+                (' celebrates outstanding films, cinema 
                   development over the past 100 year and its variety. However, 
                   the master minds behind these movies are not as manifold as 
                   the list title suggests.'),
                   br(),
                 hr(),
                 img(src="clapper2012.png"),
-                numericInput("maxpos", "The Top IMDb Movies (Maximum Entry: 250) ", 10, 1, 250),
+                numericInput("maxpos", "The Top X IMDb Movies (Maximum Entry: 250) ", 10, 1, 250),
                 br(),
                 (' were created by how many different directors?'),
                 verbatimTextOutput("FreqDirectors"),
@@ -29,11 +31,21 @@ shinyUI(fluidPage(
 #                         selected = "Dec 31, 2012" ),
 
 
-                 radioButtons("radio", label = ("based on data snapshots taken from IMDb on"),
+                ('Code on  '),
+                tags$a(href="https://github.com/cwerner13/IMDb250", "github"),
+                (', clipart from '),
+                tags$a(href="http://www.clker.com", "clker.com"),
+                (', data snapshot taken from '),
+                tags$a(href="http://www.imdb.com", "IMDb.com"),
+                ( 'on '),
+
+                 radioButtons("radio", label = (""),
                              choices = list(
 #                                          "Dec 31, 2013" = as.Date("2013-12-31", "%Y-%m-%d"),
                                            "Dec 31, 2012" = as.Date("2012-12-31", "%Y-%m-%d")), 
                              selected = as.Date("2012-12-31", "%Y-%m-%d"))
+
+
                 
                 
                 ),
